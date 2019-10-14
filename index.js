721664ce836051feebe6ff9de1cd225092465c61
kierepkae <kierepkaedyta@gmail.com>
2019-10-14 17:19:03 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const fetch = require('node-fetch');
+
+const arg = process.argv[2];
+const equalSign = arg.search('=');
+const number = arg.slice(equalSign + 1);
+let type = '';
+
+if (arg.indexOf("--year") === 0) {
+  type = 'year';
+} else if (arg.indexOf("--math") === 0) {
+  type = 'math';
+} else if (arg.indexOf("--trivia") === 0) {
+  type = 'trivia';
+};
+
+if (equalSign === -1) console.log('You have to give a number.');
+
+
+fetch(`http://numbersapi.com/${number}/${type}?json`)
+  .then(respone => respone.json())
+  .then(data => console.log(data.text))
